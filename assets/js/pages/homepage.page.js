79132e6e4718c5049d4c5cde0bc842565d2e16b8
Justin Pruskowski <justin@onsched.com>
2018-08-19 10:22:39 -0600

    Homepage using component and aax form for adding a post

--- assets/js/pages/homepage.page.js
+++ assets/js/pages/homepage.page.js
@@ -3,7 +3,21 @@ parasails.registerPage('homepage', {
   //  ║║║║║ ║ ║╠═╣║    ╚═╗ ║ ╠═╣ ║ ║╣
   //  ╩╝╚╝╩ ╩ ╩╩ ╩╩═╝  ╚═╝ ╩ ╩ ╩ ╩ ╚═╝
   data: {
+    // Main syncing/loading state for this page.
+    syncing: false,
+
+    // Form data
+    formData: { /* … */ },
+
+    // For tracking client-side validation errors in our form.
+    // > Has property set to `true` for each invalid property in `formData`.
+    formErrors: { /* … */ },
+
+    // Server error state for the form
+    cloudError: '',
+
     heroHeightSet: false,
+    dashboardModalVisible: false,
     posts: [],
   },
 
@@ -13,7 +27,13 @@ parasails.registerPage('homepage', {
   beforeMount: function() {
     // Attach any initial data from the server.
     _.extend(this, SAILS_LOCALS);
+
+    // Set the form data.
+    this.formData.title = 'sdf';
+    this.formData.url = 'http://yahoo.com';
+    this.formData.body = 'Some cool text';
   },
+
   mounted: async function(){
     this._setHeroHeight();
   },
@@ -23,11 +43,12 @@ parasails.registerPage('homepage', {
   //  ╩╝╚╝ ╩ ╚═╝╩╚═╩ ╩╚═╝ ╩ ╩╚═╝╝╚╝╚═╝
   methods: {
 
-    clickHeroButton: async function() {
-      // Scroll to the 'get started' section:
-      $('html, body').animate({
-        scrollTop: this.$find('[role="scroll-destination"]').offset().top
-      }, 500);
+    clickOpenDashboardModalButton: async function() {
+      this.dashboardModalVisible = true;
+    },
+
+    closeDashboardModal: async function() {
+      this.dashboardModalVisible = false;
     },
 
     // Private methods not tied to a particular DOM event are prefixed with _
@@ -41,5 +62,39 @@ parasails.registerPage('homepage', {
       this.heroHeightSet = true;
     },
 
+    submittedForm: async function() {
+      //Handle Twitter api call here-ish to set the body
+
+      //Close Modal once successful
+      this.closeDashboardModal();
+    },
+
+    handleParsingForm: function() {
+      //Validate Twitter data here-ish to set the body
+
+      // Clear out any pre-existing error messages.
+      this.formErrors = {};
+
+      var argins = this.formData;
+
+      // Validate title:
+      if(!argins.title) {
+        this.formErrors.title = true;
+      }
+
+      // Validate url:
+      if(!argins.url) {
+        this.formErrors.url = true;
+      }
+
+      // If there were any issues, they've already now been communicated to the user,
+      // so simply return undefined.  (This signifies that the submission should be
+      // cancelled.)
+      if (Object.keys(this.formErrors).length > 0) {
+        return;
+      }
+
+      return argins;
+    },
   }
 });
