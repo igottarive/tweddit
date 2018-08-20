2157aa760e7854271d5e0042359c624d9d853c2f
Justin Pruskowski <justin@onsched.com>
2018-08-20 13:39:27 -0600

    Added karma score and display component

---
+++ assets/js/components/postkarma.component.js
@@ -0,0 +1,73 @@
+/**
+ * <postkarma>
+ * -----------------------------------------------------------------------------
+ * Shows a post's karma score.
+ * Sum of all votes (-1, 0, 1)
+ *
+ * @type {Component}
+ *
+ * @event click   [emitted when clicked]
+ * -----------------------------------------------------------------------------
+ */
+
+parasails.registerComponent('postkarma', {
+  //  ╔═╗╦═╗╔═╗╔═╗╔═╗
+  //  ╠═╝╠╦╝║ ║╠═╝╚═╗
+  //  ╩  ╩╚═╚═╝╩  ╚═╝
+  props: [
+    'votes',
+  ],
+
+  //  ╦╔╗╔╦╔╦╗╦╔═╗╦    ╔═╗╔╦╗╔═╗╔╦╗╔═╗
+  //  ║║║║║ ║ ║╠═╣║    ╚═╗ ║ ╠═╣ ║ ║╣
+  //  ╩╝╚╝╩ ╩ ╩╩ ╩╩═╝  ╚═╝ ╩ ╩ ╩ ╩ ╚═╝
+  data: function (){
+    return {
+      karma: 0,
+    };
+  },
+
+  //  ╦ ╦╔╦╗╔╦╗╦
+  //  ╠═╣ ║ ║║║║
+  //  ╩ ╩ ╩ ╩ ╩╩═╝
+  template: `
+  <div class="postkarma">
+    <h6>Ka <span class="badge" v-bind:class="badge">{{ karma }}</span></h6>
+  </div>
+  `,
+
+  //  ╦  ╦╔═╗╔═╗╔═╗╦ ╦╔═╗╦  ╔═╗
+  //  ║  ║╠╣ ║╣ ║  ╚╦╝║  ║  ║╣
+  //  ╩═╝╩╚  ╚═╝╚═╝ ╩ ╚═╝╩═╝╚═╝
+  beforeMount: function() {
+    this.badge = 'badge-dark';
+  },
+  mounted: async function(){
+    $(this.$el).css({
+      'font-size': '.1em',
+    });
+
+    //Simple karma score algorithm
+    if(this.votes) {
+      this.karma = this.votes.reduce((a, c) => { return a + c.rating; }, 0);
+    }
+    //Set appropriate badge color
+    this.badge = this.karma < 0 ? 'badge-danger' : this.badge;
+    this.badge = this.karma > 0 ? 'badge-success' : this.badge;
+    console.log(this);
+  },
+  beforeDestroy: function() {
+    //…
+  },
+
+  //  ╦╔╗╔╔╦╗╔═╗╦═╗╔═╗╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+  //  ║║║║ ║ ║╣ ╠╦╝╠═╣║   ║ ║║ ║║║║╚═╗
+  //  ╩╝╚╝ ╩ ╚═╝╩╚═╩ ╩╚═╝ ╩ ╩╚═╝╝╚╝╚═╝
+  methods: {
+
+    click: async function(){
+      this.$emit('click');
+    },
+
+  }
+});
---
+++ test/integration/components/postkarma.test.js
@@ -0,0 +1,6 @@
+const assert = require('assert');
+describe('Postkarma Component', () => {
+  it('Lets make a test here!', () => {
+    assert(1 === 1, `number of votes match`);
+  });
+});
