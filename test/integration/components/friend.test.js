c10721603ba13c3aed897d0d428765eaf035595e
Justin Pruskowski <justin@onsched.com>
2018-08-22 04:35:09 -0600

    Make sure compoenent is loaded

--- test/integration/components/friend.test.js
+++ test/integration/components/friend.test.js
@@ -1,6 +1,9 @@
 const assert = require('assert');
+const friend = require('../../../assets/js/components/friend.component.js');
+
 describe('Friend Component', () => {
   it('Lets make a test here!', () => {
     assert(1 === 1, `number of votes match`);
+    assert(friend === friend, `silly teest`);
   });
 });
