b3b5d2d5e56ab91d08eb8693cca72d0e6245974f
Justin Pruskowski <justin@onsched.com>
2018-08-22 15:56:15 -0600

    Comment Components

---
+++ assets/js/components/add-comment.component.js
@@ -0,0 +1,86 @@
+/**
+ * <add-comment>
+ * -----------------------------------------------------------------------------
+ * A display for a post from a user.
+ *
+ * @type {Component}
+ *
+ * @event click   [emitted when clicked]
+ * -----------------------------------------------------------------------------
+ */
+if(typeof window === 'undefined') { var parasails = require('parasails'); }
+parasails.registerComponent('add-comment', {
+  //  ╔═╗╦═╗╔═╗╔═╗╔═╗
+  //  ╠═╝╠╦╝║ ║╠═╝╚═╗
+  //  ╩  ╩╚═╚═╝╩  ╚═╝
+  props: [
+    'post',
+    'user'
+  ],
+
+  //  ╦╔╗╔╦╔╦╗╦╔═╗╦    ╔═╗╔╦╗╔═╗╔╦╗╔═╗
+  //  ║║║║║ ║ ║╠═╣║    ╚═╗ ║ ╠═╣ ║ ║╣
+  //  ╩╝╚╝╩ ╩ ╩╩ ╩╩═╝  ╚═╝ ╩ ╩ ╩ ╩ ╚═╝
+  data: function (){
+    return {
+      // Main syncing/loading state for this page.
+      syncing: false,
+
+      // Form data
+      formData: { /* … */ },
+
+      // For tracking client-side validation errors in our form.
+      // > Has property set to `true` for each invalid property in `formData`.
+      formErrors: { /* … */ },
+
+      // Server error state for the form
+      cloudError: '',
+      comment: '',
+    };
+  },
+
+  //  ╦ ╦╔╦╗╔╦╗╦
+  //  ╠═╣ ║ ║║║║
+  //  ╩ ╩ ╩ ╩ ╩╩═╝
+  template: `
+  <ajax-form class="col-md-6 col-md-offset-2 col-sm-12" action="addComment" :syncing.sync="syncing" :cloud-error.sync="cloudError" :handle-parsing="handleParsingForm">
+    <div class="form-group">
+      <label for="comment">Comment</label>
+      <textarea class="form-control" id="comment" name="comment" v-model.trim="comment" placeholder="write a comment..." rows="3"></textarea>
+    </div>
+    <ajax-button type="submit" class="btn btn-outline-info pull-right">Submit</ajax-button>
+    <div class="clearfix"></div>
+  </ajax-form>
+  `,
+
+  //  ╦  ╦╔═╗╔═╗╔═╗╦ ╦╔═╗╦  ╔═╗
+  //  ║  ║╠╣ ║╣ ║  ╚╦╝║  ║  ║╣
+  //  ╩═╝╩╚  ╚═╝╚═╝ ╩ ╚═╝╩═╝╚═╝
+  beforeMount: function() {
+    //…
+  },
+  mounted: async function(){
+    $(this.$el).css({
+      'margin': '10px',
+    });
+  },
+  beforeDestroy: function() {
+    //…
+  },
+
+  //  ╦╔╗╔╔╦╗╔═╗╦═╗╔═╗╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+  //  ║║║║ ║ ║╣ ╠╦╝╠═╣║   ║ ║║ ║║║║╚═╗
+  //  ╩╝╚╝ ╩ ╚═╝╩╚═╩ ╩╚═╝ ╩ ╩╚═╝╝╚╝╚═╝
+  methods: {
+
+    handleParsingForm: function() {
+
+      this.formData.post = this.post.id;
+      this.formData.user = this.user.id;
+      this.formData.content = this.comment;
+
+      return this.formData;
+    },
+
+  }
+});
---
+++ assets/js/components/list-comment.component.js
@@ -0,0 +1,102 @@
+/**
+ * <list-comment>
+ * -----------------------------------------------------------------------------
+ * A display for a post from a user.
+ *
+ * @type {Component}
+ *
+ * @event click   [emitted when clicked]
+ * -----------------------------------------------------------------------------
+ */
+if(typeof window === 'undefined') { var parasails = require('parasails'); }
+parasails.registerComponent('list-comment', {
+  //  ╔═╗╦═╗╔═╗╔═╗╔═╗
+  //  ╠═╝╠╦╝║ ║╠═╝╚═╗
+  //  ╩  ╩╚═╚═╝╩  ╚═╝
+  props: [
+    'post',
+  ],
+
+  //  ╦╔╗╔╦╔╦╗╦╔═╗╦    ╔═╗╔╦╗╔═╗╔╦╗╔═╗
+  //  ║║║║║ ║ ║╠═╣║    ╚═╗ ║ ╠═╣ ║ ║╣
+  //  ╩╝╚╝╩ ╩ ╩╩ ╩╩═╝  ╚═╝ ╩ ╩ ╩ ╩ ╚═╝
+  data: function (){
+    return {
+      // Main syncing/loading state for this page.
+      syncing: false,
+
+      // Form data
+      formData: { /* … */ },
+
+      // For tracking client-side validation errors in our form.
+      // > Has property set to `true` for each invalid property in `formData`.
+      formErrors: { /* … */ },
+
+      // Server error state for the form
+      cloudError: '',
+      comments: [],
+    };
+  },
+
+  //  ╦ ╦╔╦╗╔╦╗╦
+  //  ╠═╣ ║ ║║║║
+  //  ╩ ╩ ╩ ╩ ╩╩═╝
+  template: `
+  <div class="col-md-6 col-md-offset-2 col-sm-12">
+    <div class="comment-wrapper">
+      <div class="panel panel-info">
+        <div class="panel-body">
+          <div class="clearfix"></div>
+          <hr>
+          <ul class="media-list">
+            <li class="media" v-for="comment in comments">
+              <img :src="'https://api.adorable.io/avatars/130/' + comment.creator.id" alt="" class="img-circle pull=left">
+              <div class="media-body">
+                <span class="text-muted pull-right">
+                  <small class="fromNow text-muted">{{ comment.fromNow }}</small>
+                </span>
+                <strong class="text-success">{{ comment.creator.fullName }}</strong>
+                <p>{{ comment.content }}</p>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+  `,
+
+  //  ╦  ╦╔═╗╔═╗╔═╗╦ ╦╔═╗╦  ╔═╗
+  //  ║  ║╠╣ ║╣ ║  ╚╦╝║  ║  ║╣
+  //  ╩═╝╩╚  ╚═╝╚═╝ ╩ ╚═╝╩═╝╚═╝
+  beforeMount: function() {
+    //Get data from server
+    $.ajax({
+      url: '/api/v1/list-comment',
+      data: {
+        post: this.post.id,
+      },
+      error: function() {
+        alert( 'this is an invalid post' );
+      },
+      success: resp => {
+        this.comments = resp;
+      },
+    });
+  },
+  mounted: async function(){
+    $(this.$el).css({
+      'margin': '10px',
+    });
+  },
+  beforeDestroy: function() {
+    //…
+  },
+
+  //  ╦╔╗╔╔╦╗╔═╗╦═╗╔═╗╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+  //  ║║║║ ║ ║╣ ╠╦╝╠═╣║   ║ ║║ ║║║║╚═╗
+  //  ╩╝╚╝ ╩ ╚═╝╩╚═╩ ╩╚═╝ ╩ ╩╚═╝╝╚╝╚═╝
+  methods: {
+
+  }
+});
