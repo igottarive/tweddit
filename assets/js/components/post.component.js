7d5e51a00e842e6849a5838ffbb692010062792d
Justin Pruskowski <justin@onsched.com>
2018-08-22 23:35:14 -0600

    Moved css to less file Added comments to post

--- assets/js/components/post.component.js
+++ assets/js/components/post.component.js
@@ -31,14 +31,24 @@ parasails.registerComponent('post', {
   //  ╠═╣ ║ ║║║║
   //  ╩ ╩ ╩ ╩ ╩╩═╝
   template: `
-  <div class="card">
-    <div class="card-body">
-      <h5 class="card-title">{{ post.title }}</h5>
-      <p class="card-text" v-html="post.body"></p>
+  <div class="post card">
+    <div class="card-body postCard">
+      <h3 class="card-title text-info">{{ post.title }}</h3>
+      <p class="card-text text-info" v-html="post.body"></p>
       <a :href="post.url" target="_blank" class="twitter btn btn-outline-info"><i class="fa fa-twitter"></i></a>
       <vote class="vote" v-bind:post="post" v-bind:user="user"></vote>
       <small class="fromNow text-muted">{{ post.fromNow }}</small>
       <postkarma class="postkarma" v-bind:votes="post.votes"></postkarma>
+      <button type="button" class="openComments btn btn-outline-info" data-toggle="collapse"
+        :data-target="'#' + id" aria-expanded="false" :aria-controls="id">
+        <i class="fa fa-comment"></i>
+      </button>
+      <div class="collapse" :id="id">
+        <div class="card card-body card-comment">
+          <add-comment v-bind:post="post" v-bind:user="user"></add-comment>
+          <list-comment v-bind:post="post"></list-comment>
+        </div>
+      </div>
     </div>
   </div>
   `,
@@ -48,32 +58,10 @@ parasails.registerComponent('post', {
   //  ╩═╝╩╚  ╚═╝╚═╝ ╩ ╚═╝╩═╝╚═╝
   beforeMount: function() {
     //…
+    this.id = 'comment' + this.post.id;
   },
   mounted: async function(){
-    $(this.$el).css({
-      'position': 'relative',
-      'margin': '10px',
-    });
-    $('.twitter').css({
-      'position': 'absolute',
-      'bottom': '5px',
-      'right': '5px',
-    });
-    $('.vote').css({
-      'position': 'absolute',
-      'top': '5px',
-      'right': '5px',
-    });
-    $('.postkarma').css({
-      'position': 'absolute',
-      'top': '5px',
-      'left': '5px',
-    });
-    $('.fromNow').css({
-      'position': 'absolute',
-      'bottom': '3px',
-      'right': '50px',
-    });
+    //…
   },
   beforeDestroy: function() {
     //…
