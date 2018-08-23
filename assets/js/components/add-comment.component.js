31d58f9235eb24ad801111491fec5483cdf5a25f
Justin Pruskowski <justin@onsched.com>
2018-08-23 12:12:18 -0600

    Don't let anon users comment

--- assets/js/components/add-comment.component.js
+++ assets/js/components/add-comment.component.js
@@ -46,9 +46,9 @@ parasails.registerComponent('add-comment', {
   <ajax-form action="addComment" :syncing.sync="syncing" :cloud-error.sync="cloudError" :handle-parsing="handleParsingForm">
     <div class="form-group">
       <label for="comment">Comments</label>
-      <textarea class="form-control" id="comment" name="comment" v-model.trim="comment" placeholder="write a comment..." rows="3"></textarea>
+      <textarea class="form-control" id="comment" name="comment" v-model.trim="comment" placeholder="write a comment..." rows="3" v-if="user"></textarea>
     </div>
-    <ajax-button type="submit" class="btn btn-outline-info pull-right">Submit</ajax-button>
+    <ajax-button type="submit" class="btn btn-outline-info pull-right" v-if="user">Submit</ajax-button>
     <div class="clearfix"></div>
   </ajax-form>
   `,
@@ -76,9 +76,7 @@ parasails.registerComponent('add-comment', {
     handleParsingForm: function() {
 
       this.formData.post = this.post.id;
-      if(this.user && this.user.id) {
-        this.formData.user = this.user.id;
-      }
+      this.formData.user = this.user.id;
       this.formData.content = this.comment;
 
       return this.formData;
